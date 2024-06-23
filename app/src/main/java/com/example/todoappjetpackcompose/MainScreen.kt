751592995cfe7744ed0648244d8cff77baa80f8a
Muhammad Abid <abidroid1@gmail.com>
2024-06-23 14:52:15 +0500

    hard coded list added

--- app/src/main/java/com/example/todoappjetpackcompose/MainScreen.kt
+++ app/src/main/java/com/example/todoappjetpackcompose/MainScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -28,6 +29,7 @@ import androidx.compose.ui.unit.dp
 fun MainScreen() {
 
     val tf = remember { mutableStateOf("") }
+    val itemsList = remember { mutableStateListOf("Learn Kotlin", "Learn Compose") }
 
     Scaffold(
         topBar = {
