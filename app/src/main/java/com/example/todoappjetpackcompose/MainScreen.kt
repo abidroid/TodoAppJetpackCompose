31cef77f7a48d8a33688d2d5b643adb2b34095b2
Muhammad Abid <abidroid1@gmail.com>
2024-06-23 15:07:25 +0500

    item content created

--- app/src/main/java/com/example/todoappjetpackcompose/MainScreen.kt
+++ app/src/main/java/com/example/todoappjetpackcompose/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.todoappjetpackcompose
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,8 +10,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -20,6 +28,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,10 +61,10 @@ fun MainScreen() {
                     .padding(paddingValues)
             ) {
 
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(7F)
-                    .padding(5.dp)
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(5.dp)
 
                 ) {
                     TextField(value = tf.value, onValueChange = {
@@ -64,7 +73,7 @@ fun MainScreen() {
                     Spacer(modifier = Modifier.width(5.dp))
                     Button(
                         modifier = Modifier
-                            .weight(3F)
+                            .weight(2F)
                             .height(60.dp),
                         onClick = { /*TODO*/ }) {
                         Text(text = "Add")
@@ -75,8 +84,42 @@ fun MainScreen() {
                 LazyColumn {
                     items(
                         count = itemsList.size,
-                        itemContent = {index ->
-                            Text(text = itemsList[index])
+                        itemContent = { index ->
+                            Card(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(60.dp)
+                                    .padding(bottom = 5.dp),
+                                shape = RoundedCornerShape(0.dp),
+
+                                onClick = { /*TODO*/ }) {
+                                Row(
+                                    modifier = Modifier.padding(5.dp),
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Text(
+                                        modifier = Modifier.weight(8F),
+                                        text = itemsList[index])
+                                    Row {
+
+                                        IconButton(
+                                            onClick = { /*TODO*/ }) {
+                                            Icon(
+                                                imageVector = Icons.Default.Delete,
+                                                contentDescription = "Edit",
+                                                tint = Color.Red
+                                            )
+                                        }
+                                        IconButton(onClick = { /*TODO*/ }) {
+                                            Icon(
+                                                imageVector = Icons.Default.Edit,
+                                                contentDescription = "Edit",
+                                                tint = Color.Blue
+                                            )
+                                        }
+                                    }
+                                }
+                            }
                         }
                     )
                 }
