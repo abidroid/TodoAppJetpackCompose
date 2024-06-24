32d494fcc78b621c5249bd83523f0d8f857c3d69
Muhammad Abid <abidroid1@gmail.com>
2024-06-24 23:28:49 +0500

    try catch added

--- app/src/main/java/com/example/todoappjetpackcompose/FileHelper.kt
+++ app/src/main/java/com/example/todoappjetpackcompose/FileHelper.kt
@@ -2,6 +2,7 @@ package com.example.todoappjetpackcompose
 
 import android.content.Context
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import java.io.FileNotFoundException
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 
@@ -19,13 +20,17 @@ fun writeToFile(items: SnapshotStateList<String>, context: Context) {
 fun readData(context: Context): SnapshotStateList<String> {
     var itemList: ArrayList<String>
 
-    val fis = context.openFileInput(FILE_NAME)
-    val ois = ObjectInputStream(fis)
-    itemList = ois.readObject() as ArrayList<String>
+    try {
+        val fis = context.openFileInput(FILE_NAME)
+        val ois = ObjectInputStream(fis)
+        itemList = ois.readObject() as ArrayList<String>
+
+    } catch (e: FileNotFoundException) {
+        itemList = ArrayList<String>()
+    }
 
     val items = SnapshotStateList<String>()
     items.addAll(itemList)
-    ois.close()
 
     return items
 }
