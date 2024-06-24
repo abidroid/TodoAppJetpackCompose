b78ce55e70a9785b48ef232ac663068fa87c2036
Muhammad Abid <abidroid1@gmail.com>
2024-06-24 22:59:04 +0500

    read data function created

--- app/src/main/java/com/example/todoappjetpackcompose/FileHelper.kt
+++ app/src/main/java/com/example/todoappjetpackcompose/FileHelper.kt
@@ -2,6 +2,7 @@ package com.example.todoappjetpackcompose
 
 import android.content.Context
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 
 const val FILE_NAME = "todo_list.dat"
@@ -13,4 +14,18 @@ fun writeToFile(items: SnapshotStateList<String>, context: Context) {
     itemList.addAll(items)
     oos.writeObject(itemList)
     oos.close()
+}
+
+fun readData(context: Context): SnapshotStateList<String> {
+    var itemList: ArrayList<String>
+
+    val fis = context.openFileInput(FILE_NAME)
+    val ois = ObjectInputStream(fis)
+    itemList = ois.readObject() as ArrayList<String>
+
+    val items = SnapshotStateList<String>()
+    items.addAll(itemList)
+    ois.close()
+
+    return items
 }
